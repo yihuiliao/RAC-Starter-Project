add32b1b337affd008811adda00a74673c09dc25
Yihui Liao <liao.yihui18@gmail.com>
2023-04-19 15:41:02 -0700

    added rac

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import {Button, Item, Label, ListBox, Popover, Select, SelectValue} from 'react-aria-components';
+
 export function App() {
-  return <h1 className="text-3xl">Hello world!</h1>;
+  return (
+    <div>
+        <h1 className="text-3xl">Hello world!</h1>
+        <Select>
+          <Label>Favorite Animal</Label>
+          <Button>
+            <SelectValue />
+            <span aria-hidden="true">▼</span>
+          </Button>
+          <Popover>
+            <ListBox>
+              <Item>Cat</Item>
+              <Item>Dog</Item>
+              <Item>Kangaroo</Item>
+            </ListBox>
+          </Popover>
+        </Select>
+    </div>
+  )
 }
