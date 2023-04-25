1ef260ba9a1a6e0d43054aed143bfeaec5c795fc
Yihui Liao <liao.yihui18@gmail.com>
2023-04-25 12:31:42 -0700

    additional css changes

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ export function Form(props){
         <Label className="mt-2">What do you need to do?</Label>
         <Input value={task} className="border-2 w-5/12 rounded-md"/>
       </TextField>
-      <Button type="submit" className="border-2 mt-3 mb-5 py-1 px-2 rounded-md">Add</Button>
+      <Button type="submit" className="border-2 mt-3 mb-5 py-1 px-2 rounded-md bg-white">Add</Button>
     </form>      
   )
 }
@@ -75,19 +75,22 @@ export function ToDoList(props) {
   tasks = props.tasks;
 
   const taskList = tasks.map((task, index) => (
-    <Checkbox value={`${task.name}-${index}`} className="flex group items-center	" key={task.id}>
-      <div className="checkbox w-5 h-5 border-2 border-solid rounded-s group-data-[selected]:bg-black mr-2" aria-hidden="true">
-        <svg viewBox="0 0 18 18" className="w-4"></svg>
+    <Checkbox value={`${task.name}-${index}`} className="flex group items-center my-1 justify-between	" key={task.id}>
+      <div className="flex">
+        <div className="checkbox w-5 h-5 border-2 border-solid rounded-s bg-white group-data-[selected]:bg-black mr-2" aria-hidden="true">
+          <svg viewBox="0 0 18 18" className="w-4"></svg>
+        </div>
+        <span className="group-data-[selected]:line-through">{task.name}</span>
       </div>
-      {task.name}
-      <EditDialog editTask={props.editTask} id={task.id}/>
-      {/* <Button onPress={() => props.editTask(task.id, newName)} className="border-2 w-auto">Edit</Button> */}
-      <Button onPress={() => props.deleteTask(task.id)} className="border-2 w-auto ml-5">Delete</Button>
+      <div className="mr-96">
+        <EditDialog editTask={props.editTask} id={task.id} className=""/>
+        <Button onPress={() => props.deleteTask(task.id)} className="border-2 w-auto ml-5 px-2 rounded-md bg-white">Delete</Button>
+      </div>
     </Checkbox>
   ))
 
   return (
-    <CheckboxGroup className="flex flex-col">
+    <CheckboxGroup className="flex flex-col w-9/12">
       <Label>To-Do List</Label>
       {taskList}
     </CheckboxGroup>
@@ -100,22 +103,24 @@ export function EditDialog(props){
 
   return (
     <DialogTrigger>
-      <Button className="ml-24">Edit</Button>
-      <Modal>
+      <Button className="ml-24 border-2 w-auto px-2 rounded-md bg-white">Edit</Button>
+      <Modal className="border-2 m-auto w-4/12 p-5 z-10 bg-white left-1/4 fixed">
         <Dialog>
         {({ close }) => (
             <form>
-              <Heading>Edit Task</Heading>
-              <TextField autoFocus onChange={setText}>
-                <Label>Updated Task</Label>
-                <Input />
+              <Heading className="text-center">Edit Task</Heading>
+              <TextField autoFocus onChange={setText} className="my-3">
+                <Label className="pr-3">Updated Task</Label>
+                <Input className="border-2 rounded-md" />
               </TextField>
-              <Button onPressStart={() => props.editTask(props.id, text)} onPress = {close} >
-                Submit
-              </Button>
-              <Button onPress = {close} >
-                Cancel
-              </Button>
+              <div className="text-right">
+                <Button onPress = {close} className="border-2 mr-3 px-2 rounded-md">
+                  Cancel
+                </Button>
+                <Button onPressStart={() => props.editTask(props.id, text)} onPress = {close} className="border-2 px-2 rounded-md">
+                  Submit
+                </Button>
+              </div>
             </form>
             )}
         </Dialog>
